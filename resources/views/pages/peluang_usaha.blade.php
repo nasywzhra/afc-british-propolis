30590f8b6f1e1d4877bd3aa9c4c6fae6a3ccf3a2
nasywa zahra nailah <nasywazahranailah@gmail.com>
2023-11-11 22:08:44 +0700

    feat(peluang usaha): page

--- resources/views/pages/peluang_usaha.blade.php
+++ resources/views/pages/peluang_usaha.blade.php
@@ -1,5 +1,48 @@
 @extends('layouts.master')
 
 @section('content')
-    Peluang usaha
+<section class="container-fluid py-5">
+    <div class="row align-items-center">
+        <div class="col-6 ps-5">
+            <p class="title-peluang-usaha">Mau bisnis dari rumah? bisnis full bimbingan?Yuk gabung bersama BP</p>
+        </div>
+        <div class="col-6 pe-5 text-end">
+            <img src="{{ asset('assets/img/pages/peluang-usaha/pu-s1.png') }}" alt="Peluang Usaha" width="75%">
+        </div>
+    </div>
+</section>
+
+<section class="container-fluid">
+    <h1 class="sub-title-peluang-usaha text-center">APA ITU BRITISH PROPOLIS?</h1>
+    <div class="row align-items">
+        <div class="col-6 ps-5">
+            <p class="content-peluang-usaha">British Propolis adalah propolis premium dari Inggris. Dikatakan premium
+                karena memang propolis ini dihasilkan langsung dari lebah madu yang hidup di berbagai musim yaitu musim
+                semi, musim panas, musim gugur dan musim salju. Beberapa riset mengatakan bahwa lebah terbaik adalah
+                lebah yang bisa hidup di 4 musim, termasuk Inggris. Di Indonesia sendiri, BP termasuk propolis kualitas
+                terbaik dan rekomendasi tokoh.</p>
+        </div>
+        <div class="col-6 pe-5">
+            <img src="{{ asset('assets/img/pages/peluang-usaha/pu-s2.png') }}" alt="Apa itu british propolis?"
+                width="75%">
+        </div>
+    </div>
+</section>
+
+<section class="container-fluid py-5">
+    <h1 class="sub-title-peluang-usaha text-center">APA ITU PELUANG USAHA?</h1>
+    <div class="row align-items-center">
+        <div class="col-6 ps-5">
+            <img src="{{ asset('assets/img/pages/peluang-usaha/pu-s3.png') }}" alt="Apa itu british propolis? 2"
+                width="100%">
+        </div>
+        <div class="col-6 pe-5">
+            <p class="content-peluang-usaha">British Propolis adalah propolis premium dari Inggris. Dikatakan premium
+                karena memang propolis ini dihasilkan langsung dari lebah madu yang hidup di berbagai musim yaitu musim
+                semi, musim panas, musim gugur dan musim salju. Beberapa riset mengatakan bahwa lebah terbaik adalah
+                lebah yang bisa hidup di 4 musim, termasuk Inggris. Di Indonesia sendiri, BP termasuk propolis kualitas
+                terbaik dan rekomendasi tokoh.</p>
+        </div>
+    </div>
+</section>
 @endsection
