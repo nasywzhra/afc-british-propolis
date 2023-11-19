c6a8eaae45035ab8e30482049c4802c8bb17f7b3
nasywa zahra nailah <nasywazahranailah@gmail.com>
2023-11-19 21:02:17 +0700

    feat(peluang-usaha) section cara bergabung

--- resources/views/pages/peluang_usaha.blade.php
+++ resources/views/pages/peluang_usaha.blade.php
@@ -34,46 +34,49 @@
     <div class="row mb-5">
         <div class="col-4">
             <div class="card">
-                <div class="card-body">
+                <div class="card-body business-process">
                     <div class="row">
                         <div class="col-2 text-center px-0">
                             <img src="{{ asset('assets/img/pages/peluang-usaha/num-1.png') }}" alt="1" class="w-50">
                         </div>
                         <div class="col-10 ps-0">
                             <p class="content-peluang-usaha mb-0">
-                                    Pertama, dapatkan harga spesial yang lebih murah dengan menjadi mitra. Anda bisa hemat konsumsi produknya dan bisa dijual kembali                       
+                                Pertama, dapatkan harga spesial yang lebih murah dengan menjadi mitra. Anda bisa hemat
+                                konsumsi produknya dan bisa dijual kembali
                             </p>
-                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
         <div class="col-4">
             <div class="card">
-                <div class="card-body">
+                <div class="card-body business-process">
                     <div class="row">
                         <div class="col-2 text-center px-0">
                             <img src="{{ asset('assets/img/pages/peluang-usaha/num-2.png') }}" alt="2" class="w-50">
                         </div>
                         <div class="col-10 ps-0">
                             <p class="content-peluang-usaha mb-0">
-                                    Kedua, Anda perkenalkan manfaat British Propolis, kepada pelanggan Anda atau calon pembeli secara offline dan online                        
-                                </p>
-                            </div>
+                                Kedua, Anda perkenalkan manfaat British Propolis, kepada pelanggan Anda atau calon
+                                pembeli secara offline dan online
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
         <div class="col-4">
             <div class="card">
-                <div class="card-body">
+                <div class="card-body business-process">
                     <div class="row">
                         <div class="col-2 text-center px-0">
                             <img src="{{ asset('assets/img/pages/peluang-usaha/num-3.png') }}" alt="3" class="w-50">
                         </div>
                         <div class="col-10 ps-0">
                             <p class="content-peluang-usaha mb-0">
-                                    Ketiga, ikuti arahan dan bimbingan mentor agar perjalanan untuk sukses Anda lebih cepat dan mudah.
+                                Ketiga, ikuti arahan dan bimbingan mentor agar perjalanan untuk sukses Anda lebih cepat
+                                dan mudah.
                             </p>
                         </div>
                     </div>
@@ -86,68 +89,154 @@
 <section class="container-fluid">
     <h1 class="sub-title-peluang-usaha text-center px-5">MENGAPA HARUS BERGABUNG BISNIS BERSAMA BRITISH PROPOLIS?</h1>
     <div class="row mb-5">
-        <div class="col-4 text-center px-4" >
+        <div class="col-4 text-center px-4">
             <div>
-                <img src= "{{ asset('assets/img/pages/peluang-usaha/down.png') }}" width="50px" alt="">
+                <img src="{{ asset('assets/img/pages/peluang-usaha/down.png') }}" width="50px" alt="">
             </div>
             <div>
                 <p class="content-peluang-usaha mb-0"><strong>Modal Awal Kecil</strong></p>
                 <p class="content-peluang-usaha mb-0">
-                    Modal awal relatif kecil, namun Anda tetap memiliki kesempatan berbisnis dengan dimentori oleh motivator hebat serta tokoh terkemuka di Indonesia.                </p>
+                    Modal awal relatif kecil, namun Anda tetap memiliki kesempatan berbisnis dengan dimentori oleh
+                    motivator hebat serta tokoh terkemuka di Indonesia. </p>
             </div>
         </div>
-        <div class="col-4 text-center px-4" >
+        <div class="col-4 text-center px-4">
             <div>
-                <img src= "{{ asset('assets/img/pages/peluang-usaha/down.png') }}" width="50px" alt="">
+                <img src="{{ asset('assets/img/pages/peluang-usaha/down.png') }}" width="50px" alt="">
             </div>
             <div>
                 <p class="content-peluang-usaha mb-0"><strong>Selalu Ready Stock</strong></p>
                 <p class="content-peluang-usaha mb-0">
-                    Produk kami selalu ready stock karena diproduksi oleh pabrik besar. Anda tidak perlu pusing akibat stok yang langka.                </p>
+                    Produk kami selalu ready stock karena diproduksi oleh pabrik besar. Anda tidak perlu pusing akibat
+                    stok yang langka. </p>
             </div>
         </div>
-        <div class="col-4 text-center px-4" >
+        <div class="col-4 text-center px-4">
             <div>
-                <img src= "{{ asset('assets/img/pages/peluang-usaha/down.png') }}" width="50px" alt="">
+                <img src="{{ asset('assets/img/pages/peluang-usaha/down.png') }}" width="50px" alt="">
             </div>
             <div>
                 <p class="content-peluang-usaha mb-0"><strong>Mentoring dan Coaching</strong></p>
                 <p class="content-peluang-usaha mb-0">
-                    Jika Anda pemula dalam berbisnis, pada tahap awal secara personal Anda akan dibimbing dan diberikan tahapan-tahapan mengenai hal-hal yang harus dilakukan hingga Anda ahli dalam bisnis British Propolis.</p>
+                    Jika Anda pemula dalam berbisnis, pada tahap awal secara personal Anda akan dibimbing dan diberikan
+                    tahapan-tahapan mengenai hal-hal yang harus dilakukan hingga Anda ahli dalam bisnis British
+                    Propolis.</p>
             </div>
         </div>
     </div>
     <div class="row">
-        <div class="col-4 text-center px-4" >
+        <div class="col-4 text-center px-4">
             <div>
-                <img src= "{{ asset('assets/img/pages/peluang-usaha/down.png') }}" width="50px" alt="">
+                <img src="{{ asset('assets/img/pages/peluang-usaha/down.png') }}" width="50px" alt="">
             </div>
             <div>
                 <p class="content-peluang-usaha mb-0"><strong>Rutin Seminar</strong></p>
                 <p class="content-peluang-usaha mb-0">
-                    Seminar serta pelatihan diadakan secara rutin oleh Ippho Santosa selaku founder British Propolis dengan menggandeng para motivator serta entrepreneur​ terkemuka negeri ini, sehingga dapat menambah kemampuan berbisnis Anda.                </p>
+                    Seminar serta pelatihan diadakan secara rutin oleh Ippho Santosa selaku founder British Propolis
+                    dengan menggandeng para motivator serta entrepreneur​ terkemuka negeri ini, sehingga dapat menambah
+                    kemampuan berbisnis Anda. </p>
                 </p>
             </div>
         </div>
-        <div class="col-4 text-center px-4" >
+        <div class="col-4 text-center px-4">
             <div>
-                <img src= "{{ asset('assets/img/pages/peluang-usaha/down.png') }}" width="50px" alt="">
+                <img src="{{ asset('assets/img/pages/peluang-usaha/down.png') }}" width="50px" alt="">
             </div>
             <div>
                 <p class="content-peluang-usaha mb-0"><strong>Repeat Order Tinggi</strong></p>
                 <p class="content-peluang-usaha mb-0">
-                    Produk dengan repeat order yang sangat tinggi. Pelanggan bisa terus membeli dengan jangka waktu yang singkat                </p>
+                    Produk dengan repeat order yang sangat tinggi. Pelanggan bisa terus membeli dengan jangka waktu yang
+                    singkat </p>
             </div>
         </div>
-        <div class="col-4 text-center px-4" >
+        <div class="col-4 text-center px-4">
             <div>
-                <img src= "{{ asset('assets/img/pages/peluang-usaha/down.png') }}" width="50px" alt="">
+                <img src="{{ asset('assets/img/pages/peluang-usaha/down.png') }}" width="50px" alt="">
             </div>
             <div>
                 <p class="content-peluang-usaha mb-0"><strong>Kesempatan jadi Distributor British Propolis</strong></p>
                 <p class="content-peluang-usaha mb-0">
-                    Walaupun Anda memulai bisnis BP ini dari level Reseller atau Agen British Propolis. Namun, Anda diberi berkesempatan untuk jadi Distributor British Propolis juga. Peluangnya sangat besar!            </div>
+                    Walaupun Anda memulai bisnis BP ini dari level Reseller atau Agen British Propolis. Namun, Anda
+                    diberi berkesempatan untuk jadi Distributor British Propolis juga. Peluangnya sangat besar!
+            </div>
         </div>
+    </div>
+</section>
+
+<section class="pt-5">
+    <div class="d-flex justify-content-center">
+    <img src="{{ asset('assets/img/pages/peluang-usaha/line.png') }}" width="65%" alt="line">
+    </div>
+    <div>
+        <h1 class="sub-title-peluang-usaha text-center mt-3">BAGAIMANA CARA BERGABUNGNYA?</h1>
+        <p class="content-peluang-usaha text-center">Apabila ingin menjadi mitra BP, anda hanya perlu investasi paket kemitraan dibawah ini: </p>
+    </div>
+    <div class="price-list-peluang-usaha">
+        <div class="container py-5">
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-1 text-end">
+                        <img src="{{ asset('assets/img/pages/peluang-usaha/num-1.png') }}" alt="1" width="45px">
+                    </div>
+                    <div class="col-11">
+                        <p class="content-peluang-usaha fw-bold">Rp. 650k</p>
+                        <p class="content-peluang-usaha fw-semibold">PAKET RESELLER</p>
+                        <ul class="mb-0">
+                            <li class="content-peluang-usaha">Mendapatkan 3 botol BP (all varian) @Rp. 250.000</li>
+                            <li class="content-peluang-usaha">Margin keuntungan Rp. 100.000</li>
+                            <li class="content-peluang-usaha">Free digital promotion kit dan marketing kit dasar</li>
+                            <li class="content-peluang-usaha">Web support bisnis (senilai 1 juta)</li>
+                            <li class="content-peluang-usaha">Reward</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card my-5">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-1 text-end">
+                        <img src="{{ asset('assets/img/pages/peluang-usaha/num-2.png') }}" alt="2" width="45px">
+                    </div>
+                    <div class="col-11">
+                        <p class="content-peluang-usaha fw-bold">Rp. 990k</p>
+                        <p class="content-peluang-usaha fw-semibold">PAKET AGEN</p>
+                        <ul class="mb-0">
+                            <li class="content-peluang-usaha">Mendapatkan 5 botol BP (all varian) @Rp. 250.000</li>
+                            <li class="content-peluang-usaha">Margin keuntungan Rp. 260.000</li>
+                            <li class="content-peluang-usaha">Free digital promotion kit dan marketing kit dasar</li>
+                            <li class="content-peluang-usaha">Web support bisnis (senilai 1 juta)</li>
+                            <li class="content-peluang-usaha">Free E-Course IG marketing moduls (senilai 399rb), E-Course Cara Menulis Iklan yang Menjual (199rb), E-Course WhatsApp Marketing Moduls (senilai 399rb), E-Course FB marketing moduls (senilai 499rb)</li>
+                            <li class="content-peluang-usaha">Reward</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-1 text-end">
+                        <img src="{{ asset('assets/img/pages/peluang-usaha/num-3.png') }}" alt="3" width="45px">
+                    </div>
+                    <div class="col-11">
+                        <p class="content-peluang-usaha fw-bold">Rp. 650k</p>
+                        <p class="content-peluang-usaha fw-semibold">PAKET AGEN PLUS</p>
+                        <ul class="mb-0">
+                            <li class="content-peluang-usaha">Mendapatkan 10 botol BP (all varian) @Rp. 250.000</li>
+                            <li class="content-peluang-usaha">Margin keuntungan Rp. 700.000</li>
+                            <li class="content-peluang-usaha">Free digital promotion kit dan marketing kit dasar</li>
+                            <li class="content-peluang-usaha">Web support bisnis (senilai 1 juta)</li>
+                            <li class="content-peluang-usaha">Free modul yang ada dalam paket Agen, ditambah Modul Google Ads (senilai 199rb) dan SEO Domination (senilai 199rb)</li>
+                            <li class="content-peluang-usaha">Landingpage Bisnis</li>
+                            <li class="content-peluang-usaha">Reward</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     </div>
 </section>
 @endsection
